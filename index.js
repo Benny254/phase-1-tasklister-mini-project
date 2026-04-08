725b0d08472157128d8c87f70f55127cd9590fd1
Benny <asshimbenny01@gmail.com>
2026-04-08 13:34:24 +0300

    added an event to the form and add input to webpage

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-//Select Form
-//The form is under the id of create-task-form
-//Add event listener to form
-//Add submit for type of event
-//Add arrow function
-//Add preventDefault()
-
 const form = document.querySelector('#create-task-form')
 form.addEventListener('submit', (event) => {
     event.preventDefault()
 })
-
-//HANDLE FORM DATA  
-//Using event gather data into an object
-//Use the name new-task-description to select the correct target from the event, and save to a variable.
-//Create a new function called buildToDo() and call it.
-//Pass in the saved task.
-
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#create-task-form')
   const input = document.querySelector('#new-task-description')
@@ -29,8 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!taskText) return
 
     const li = document.createElement('li')
-
-    // ✅ THIS IS THE MISSING LINE
     li.textContent = taskText
 
     list.appendChild(li)
